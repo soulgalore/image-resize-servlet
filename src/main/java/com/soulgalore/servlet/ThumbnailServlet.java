ce5ec6c1c065756dae4c55e584175d75ebbf7ea5
Peter Hedenskog <peter@soulgalore.com>
2012-06-21 11:40:28 +0200

    added doc

--- src/main/java/com/soulgalore/servlet/ThumbnailServlet.java
+++ src/main/java/com/soulgalore/servlet/ThumbnailServlet.java
@@ -12,6 +12,30 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.common.collect.ImmutableSet;
 
+/**
+ * Example of a imagemagick convert servlet. Will convert an already existing
+ * image to a configured size, accessed size.
+ * 
+ * User scenario:
+ * <ol>
+ * <li>Access the servlet with a specified name:
+ * /SERVLET/?img=MY_ORIGINAL_IMAGE-120x94.png</li>
+ * <li>The servlet will check if the original image exist (named
+ * MY_ORIGINAL_IMAGE.png)</li>
+ * <li>The servet will check if that size of the image already exist on disk, if
+ * it exist, it will be returned
+ * <li>
+ * <li>If the images don't exist, the size will be created using imagemagick if
+ * the size is configured as a {@link #VALID_SIZES}.</li>
+ * <li>The image is returned</li>
+ * </ul> No cache header is added within the servlet. Note also that the
+ * imagemagick needs to be in the path of the user that's starts the servlet
+ * runner.
+ * 
+ * 
+ * 
+ * 
+ */
 public class ThumbnailServlet extends HttpServlet {
 
 	/**
@@ -35,13 +59,12 @@ public class ThumbnailServlet extends HttpServlet {
 	 * The web base dir of all thumbnails.
 	 */
 	private static final String THUMB_WEB_DIR = "thumbs/";
-	
+
 	/**
 	 * The web base dir of all original images.
 	 */
 	private static final String ORIGINAL_WEB_DIR = "originals/";
-	
-	
+
 	/**
 	 * Where the original image will be located, need to be changed in a love
 	 * environment.
@@ -56,21 +79,21 @@ public class ThumbnailServlet extends HttpServlet {
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		
-		 destinationBaseDir = getServletContext().getRealPath(
-					"/" + THUMB_WEB_DIR);
-		 
-		 originalBaseDir = getServletContext().getRealPath("/")
-					+ ORIGINAL_WEB_DIR;
+
+		destinationBaseDir = getServletContext().getRealPath(
+				"/" + THUMB_WEB_DIR);
+
+		originalBaseDir = getServletContext().getRealPath("/")
+				+ ORIGINAL_WEB_DIR;
 	}
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
 		// fetch filename
 		String filename = req.getParameter(IMG_REQUEST_PARAMETER);
-		
+
 		if (!isRequestValid(filename, resp))
 			return;
 
@@ -113,34 +136,34 @@ public class ThumbnailServlet extends HttpServlet {
 					"Not a valid image size");
 			return false;
 		} else
-		// TODO add regexp to verify filename standard
-		return true;
+			// TODO add regexp to verify filename standard
+			return true;
 	}
 
 	private void returnTheFile(HttpServletRequest req,
-			HttpServletResponse resp, String pathToFile) throws ServletException,
-			IOException {
+			HttpServletResponse resp, String pathToFile)
+			throws ServletException, IOException {
 		RequestDispatcher rd = getServletContext().getRequestDispatcher(
-				"/"+ THUMB_WEB_DIR + pathToFile);
+				"/" + THUMB_WEB_DIR + pathToFile);
 		rd.forward(req, resp);
 	}
 
-	private void createThumbnail(String filename) throws InterruptedException, IOException {
+	private void createThumbnail(String filename) throws InterruptedException,
+			IOException {
 
-		File dir = new File(destinationBaseDir
-				+ getGeneratedFilePath(filename));
+		File dir = new File(destinationBaseDir + getGeneratedFilePath(filename));
 
 		if (!dir.exists())
 			dir.mkdirs();
 
 		String originalName = getOriginalFileName(filename);
-		
+
 		ProcessBuilder pb = new ProcessBuilder("convert", "-thumbnail",
-				getAskedFileSize(filename), originalBaseDir + originalName + getFileEnding(filename),
-				destinationBaseDir + getGeneratedFilePath(filename)
-						+ originalName + "-" + getAskedFileSize(filename)
-						+ getFileEnding(filename));
-		
+				getAskedFileSize(filename), originalBaseDir + originalName
+						+ getFileEnding(filename), destinationBaseDir
+						+ getGeneratedFilePath(filename) + originalName + "-"
+						+ getAskedFileSize(filename) + getFileEnding(filename));
+
 		pb.directory(new File(originalBaseDir));
 		try {
 			Process p = pb.start();
@@ -157,11 +180,11 @@ public class ThumbnailServlet extends HttpServlet {
 	private String getOriginalFileName(String filename) {
 		return filename.substring(0, filename.lastIndexOf("-"));
 	}
-	
+
 	private String getFileEnding(String filename) {
 		return filename.substring(filename.lastIndexOf("."), filename.length());
 	}
-	
+
 	private String getAskedFileSize(String filename) {
 		return filename.substring(filename.lastIndexOf("-") + 1,
 				filename.lastIndexOf("."));
